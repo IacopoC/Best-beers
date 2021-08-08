8669674cb48ea2684afb7cebc77871a9d242dacb
39340 <iacopo.cutino@gmail.com>
2021-08-08 20:57:49 +0200

    creata tabella database beers

---
+++ database/migrations/2021_08_08_185203_create_beers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('tagline');
+            $table->string('count_drink');
+            $table->string('drunk');
+            $table->timestamps();
+            $table->integer('users_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
