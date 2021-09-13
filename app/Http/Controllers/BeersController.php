b9f870764d5f54924268360ecde929f18cd7a4a8
39340 <iacopo.cutino@gmail.com>
2021-09-13 20:48:49 +0200

    modifiche query birra salvata view

--- app/Http/Controllers/BeersController.php
+++ app/Http/Controllers/BeersController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Beer;
+use Illuminate\Support\Facades\Auth;
 
 class BeersController extends Controller
 {
@@ -24,13 +26,13 @@ class BeersController extends Controller
         $singleBeer = $this->basetype->getSingleBeer($id);
         $count_beer = $this->getCountedBeer($id);
 
-        return view('single-beer', compact( 'singleBeer'));
+        return view('single-beer', compact( 'singleBeer','count_beer'));
     }
 
     public function getCountedBeer($id) {
         $user_id = Auth::user()->id;
         $counted_beer = Beer::where('beers_id', $id)->where('users_id', $user_id)->get();
-        
+
         return $counted_beer;
     }
 
