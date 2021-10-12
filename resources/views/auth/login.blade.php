fe4051e718c3928b239f374e37a4f8abf8061181
39340 <iacopo.cutino@gmail.com>
2021-10-12 21:11:11 +0200

    Modifiche stile login e register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,14 +23,13 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 mb-150">
-                <div class="card">
 
-                    <div class="card-body bg-warning">
+                    <div>
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="form-group row pb-2">
-                                <label for="email" class="col-md-4 col-form-label text-white text-md-right">{{ __('E-Mail') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -44,7 +43,7 @@
                             </div>
 
                             <div class="form-group row pb-2">
-                                <label for="password" class="col-md-4 col-form-label text-white text-md-right">{{ __('Password') }}</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -62,32 +61,31 @@
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                        <label class="form-check-label text-white" for="remember">
+                                        <label class="form-check-label" for="remember">
                                             {{ __('Ricordami') }}
                                         </label>
                                     </div>
                                 </div>
                             </div>
 
-                            <div class="form-group row mb-0">
+                            <div class="form-group row mt-4 mb-4">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-warning">
                                         {{ __('Login') }}
                                     </button>
 
                                     @if (Route::has('password.request'))
-                                        <a class="btn btn-link text-white" href="{{ route('password.request') }}">
+                                        <a class="btn btn-link text-danger" href="{{ route('password.request') }}">
                                             {{ __('Password dimenticata?') }}
                                         </a>
                                     @endif
                                 </div>
                             </div>
                         </form>
-                        <p class="text-white">User: iacopo.cutino@gmail.com</p>
-                        <p class="text-white">Pass: ciaociao1</p>
+                        <p>User: iacopo.cutino@gmail.com</p>
+                        <p>Pass: ciaociao1</p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,14 +22,13 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8 mb-150">
-                <div class="card">
 
-                    <div class="card-body bg-warning">
+                    <div>
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
                             <div class="form-group row pb-2">
-                                <label for="name" class="col-md-4 col-form-label text-md-right text-white">{{ __('Nome') }}</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -43,7 +42,7 @@
                             </div>
 
                             <div class="form-group row pb-2">
-                                <label for="email" class="col-md-4 col-form-label text-md-right text-white">{{ __('E-Mail') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -57,7 +56,7 @@
                             </div>
 
                             <div class="form-group row pb-2">
-                                <label for="password" class="col-md-4 col-form-label text-md-right text-white">{{ __('Password') }}</label>
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -71,7 +70,7 @@
                             </div>
 
                             <div class="form-group row pb-2">
-                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right text-white">{{ __('Conferma Password') }}</label>
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Conferma Password') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -80,7 +79,7 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-warning">
                                         {{ __('Registrati') }}
                                     </button>
                                 </div>
@@ -90,5 +89,4 @@
                 </div>
             </div>
         </div>
-    </div>
 @endsection
