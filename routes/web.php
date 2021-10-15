57a3e78bdf174631e6551829a543982ba09cbbf7
39340 <iacopo.cutino@gmail.com>
2021-10-15 10:29:36 +0200

    Impostazione verifica email

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BeersController;
 use App\Http\Controllers\BeerscountController;
 use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,7 +26,7 @@ Route::get('/beers/{page}', [BeersController::class, 'index']);
 
 Route::get('/single-beer/{id}', [BeersController::class, 'show']);
 
-Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth','verified'])->name('dashboard');
 
 Route::get('/your-beer', [BeerscountController::class, 'show'])->middleware(['auth'])->name('your-beer');
 
@@ -35,5 +37,24 @@ Route::post('/dashboard', [UserController::class, 'update']);
 Route::post('/single-beer/{id}', [BeerscountController::class, 'create']);
 
 Route::patch('/single-beer/{id}', [BeerscountController::class, 'update']);
+
+
+/*  Email verification routes */
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 require __DIR__.'/auth.php';
