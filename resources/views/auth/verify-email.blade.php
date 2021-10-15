a755affef2528352a0d045a87fa7b8be8f6799ea
39340 <iacopo.cutino@gmail.com>
2021-10-15 10:03:14 +0200

    Modifica vista verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,14 +1,27 @@
 @extends('layout')
 
 @section('title')
-    Verifica email - Best Beers
+    Verify email - Best Beers
 @endsection
 
 @section('content')
+    <header class="py-5 mb-5 bg-warning">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <h1 class="display-4 text-white mt-5 mb-2">Verify email</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card mb-150">
                     <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                     <div class="card-body">
