37ed1c9dbed20cab311d9c4c8e4e8d5241a65cc0
39340 <iacopo.cutino@gmail.com>
2021-12-05 21:15:28 +0100

    modifiche homepage e stile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,19 @@
 @endsection
 
 @section('content')
-    <header class="py-5 mb-5 bg-warning">
+    <header class="py-5">
         <div class="container h-100">
-            <div class="row h-100 align-items-center">
-                <div class="col-lg-12">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h1 class="display-4 text-white mt-5 mb-2">Welcome to Best Beers</h1>
-                            <p class="text-white">Happy surfing</p>
-                        </div>
-                    </div>
-                </div>
+            <div class="full-width">
+                <img src="{{ asset('img/header.jpg') }}" class="img-fluid" alt="header">
             </div>
         </div>
     </header>
     <div class="container">
-        <div class="row mb-3">
-            <div class="col-md-6">
-                <h3>Functionality of Best Beers</h3>
-               <ul class="pt-4">
+        <div class="row mb-3 mt-3">
+            <div class="col-md-7">
+                <h3 class="pt-4">Welcome to Best Beers: archive of the brewdog beers</h3>
+                <p class="pt-4">Functionality of Best Beers Website</p>
+               <ul class="pt-2">
                    <li>Login user</li>
                    <li>See all the beer in Brewdog catalogue</li>
                    <li>Register user</li>
@@ -35,9 +29,20 @@
                 </div>
                 @endguest
             </div>
-            <div class="col-md-6">
-                <img class="img-fluid" src="{{ asset('img/beers.jpg') }}" alt="birre">
+            <div class="col-md-5">
+                <img class="img-fluid" src="{{ asset('img/beers.jpg') }}" alt="beers">
             </div>
         </div>
+        <hr class="divider">
+        <div class="row mb-3 mt-3">
+            <div class="col-md-5">
+                <img class="img-fluid" src="{{ asset('img/beer-pub.jpg') }}" alt="pub">
+            </div>
+            <div class="col-md-7">
+                <h3 class="pt-4">History of Brewdog</h3>
+                <p class="pt-4">BrewDog is a multinational brewery and pub chain based in Ellon, Scotland. It was founded in 2007 by James Watt and Martin Dickie. Together they own 46% of the company.</p>
+            </div>
+        </div>
+        <div class="divider"></div>
     </div>
 @endsection
